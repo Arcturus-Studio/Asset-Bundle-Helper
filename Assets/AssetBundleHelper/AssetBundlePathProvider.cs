c998a8ed7d823a89e26dc8863da8504289d4ac39
Gabriel Sibley <gabrielsibley@gmail.com>
2014-09-17 13:58:48 -0300

    Improved default path provider

--- Assets/AssetBundleHelper/AssetBundlePathProvider.cs
+++ Assets/AssetBundleHelper/AssetBundlePathProvider.cs
@@ -3,23 +3,41 @@ using System.Collections;
 using System.IO;
 
 public class AssetBundlePathProvider {
-
+	private string cachedPath;
+	
 	public virtual string GetPath(){
+		if(cachedPath != null){
+			return cachedPath;
+		}
+		
+		string fileProtocol = UseTripleSlashFileProtocol ? "file:///" : "file://";
+		#if UNITY_EDITOR
 		//Editors: Load out of assets directory
-		if (	Application.platform == RuntimePlatform.OSXEditor
-			|| Application.platform == RuntimePlatform.WindowsEditor){
-            return "file://" + Path.Combine(Directory.GetCurrentDirectory(), "Bundles"); //TODO: Not a magic string
+		if (Application.isEditor)
+		{
+			cachedPath = fileProtocol + Path.Combine(Directory.GetCurrentDirectory(), AssetBundleHelperSettings.GetInstance().bundleDirectoryRelativeToProjectFolder);            
 		}
-		else if (Application.platform == RuntimePlatform.WindowsWebPlayer
-				|| Application.platform == RuntimePlatform.OSXWebPlayer){
-			return Application.streamingAssetsPath;
+		else
+		#endif
+		if (Application.isWebPlayer){
+			cachedPath = Application.streamingAssetsPath;
 		}
-		else if (Application.platform == RuntimePlatform.PS3 	//???
-			|| Application.platform == RuntimePlatform.XBOX360  //???
-			|| Application.platform == RuntimePlatform.WindowsPlayer
-			|| Application.platform == RuntimePlatform.OSXPlayer){
-            return "file://" + Application.streamingAssetsPath;
+		else {
+            cachedPath = fileProtocol + Application.streamingAssetsPath;
 		}
-		throw new System.Exception("Unsupported runtime platform " + Application.platform);
+		
+		return cachedPath;
+	}
+	
+	private bool UseTripleSlashFileProtocol{
+		get{
+			return Application.platform == RuntimePlatform.WindowsPlayer
+			|| Application.platform == RuntimePlatform.WindowsWebPlayer
+			|| Application.platform == RuntimePlatform.WindowsEditor
+			|| Application.platform == RuntimePlatform.WSAPlayerX86
+			|| Application.platform == RuntimePlatform.WSAPlayerX64
+			|| Application.platform == RuntimePlatform.WSAPlayerARM;
+			//TODO: Also Windows phone?
+		}
 	}
 }
