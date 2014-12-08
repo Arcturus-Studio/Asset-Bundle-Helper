5dfdc04c72d967dc52ff23ab875903df2488d902
Gabriel Sibley <gabrielsibley@gmail.com>
2014-12-08 14:39:06 -0400

    Moved AssetBundleContentsWeakReference into its own file. Additional minor de-magicking of platform-inclusive tag masking

---
+++ Assets/AssetBundleHelper/Scripts/AssetBundleContentsWeakReference.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using System.IO;
+#endif
+
+/* Manages a weak reference to an AssetBundleContents asset. AssetBundleListings are meant to be used at runtime,
+	so they cannot strongly reference the AssetBundleContents without dragging them into the build.*/
+[System.Serializable]
+public class AssetBundleContentsWeakReference {
+	public string tags; //Period-delimited tag set string
+
+#if UNITY_EDITOR
+	public string ContentsPath {
+		get { return contentsPath; }		
+		set{ contentsPath = value; }
+	}
+#endif
+	[SerializeField] private string contentsPath; //Path to AssetBundleContents asset
+
+#if UNITY_EDITOR
+	public AssetBundleContents Load(){
+		return AssetDatabase.LoadMainAssetAtPath(contentsPath) as AssetBundleContents;
+	}
+	
+	public AssetBundleContents LoadOrCreate(AssetBundleListing sourceListing){
+		if(string.IsNullOrEmpty(contentsPath)){
+			Create(sourceListing);
+		}
+		AssetBundleContents loadedContents = Load();
+		if(!loadedContents){
+			Debug.LogWarning("Missing AssetBundleContents at \"" + contentsPath + "\". A replacement will be created.");
+			Create(sourceListing);
+			loadedContents = Load();
+		}
+		else{
+			System.Console.WriteLine("Loaded AssetBundleContents at " + contentsPath + " for " + sourceListing + " with tags \"" + tags + "\"");
+		}
+		return loadedContents;
+	}
+	
+	public void Create(AssetBundleListing sourceListing){
+		System.Console.WriteLine("Creating AssetBundleContents for " + sourceListing + " with tags \"" + tags + "\"");
+		contentsPath = GetPath(sourceListing.Id, tags);
+		if(!Directory.Exists(Path.GetDirectoryName(contentsPath))){
+			Directory.CreateDirectory(Path.GetDirectoryName(contentsPath));
+		}
+		AssetBundleContents	contents = ScriptableObject.CreateInstance<AssetBundleContents>();
+		contents.listing = sourceListing;
+		contents.tags = tags;
+		AssetDatabase.CreateAsset(contents, contentsPath);
+		EditorUtility.SetDirty(sourceListing);
+		AssetDatabase.SaveAssets();
+	}
+	
+	//Returns the target path for a contents with the given listing ID and tag set.
+	public static string GetPath(string listingId, string tags){
+		string dir = Path.Combine(AssetBundleEditorSettings.DirectoryPath, "BundleContents");
+		return Path.Combine(dir, listingId + (string.IsNullOrEmpty(tags) ? "" : AssetBundleChars.BundleSeparator.ToString()) + tags + ".asset");
+	}
+#endif
+}
--- Assets/AssetBundleHelper/Scripts/AssetBundleListing.cs
+++ Assets/AssetBundleHelper/Scripts/AssetBundleListing.cs
@@ -26,6 +26,7 @@ public class AssetBundleListing : ScriptableObject {
 	public List<AssetBundleContentsWeakReference> contentWeakRefs = new List<AssetBundleContentsWeakReference>();
 	public List<AssetBundleListing> dependencies = new List<AssetBundleListing>();
 	
+	//Unique identifier for this listing, used for constructing file paths for asset bundles and AssetBundleContents
 	public string Id {
 		get { return id; }
 #if UNITY_EDITOR
@@ -35,7 +36,7 @@ public class AssetBundleListing : ScriptableObject {
 		}
 #endif
 	}
-	[SerializeField] private string id; //Unique identifier for this listing, used for constructing file paths for asset bundles and AssetBundleContents
+	[SerializeField] private string id;
 	
 	//Fetch an asset contained in this listing by name.
 	//Warning: If you do not match GetAsset calls with ReleaseAsset calls, the asset will never be unloaded.
@@ -87,7 +88,7 @@ public class AssetBundleListing : ScriptableObject {
 	//As SelectedTagGroups, but forces inclusion of the Platform tag group.
 	public List<BundleTagGroup> SelectedTagGroupsForcePlatformGroup{
 		get{
-			return AssetBundleEditorSettings.GetInstance().MaskToTagGroups(tagMask | 1);
+			return AssetBundleEditorSettings.GetInstance().MaskToTagGroups(TagMaskIncludingPlatform);
 		}
 	}
 	
@@ -112,63 +113,6 @@ public class AssetBundleListing : ScriptableObject {
 			contentWeakRefs.Add(weakRef);
 		}
 		return weakRef.LoadOrCreate(this);
-	}
-#endif
-}
-
-/* Manages a weak reference to an AssetBundleContents asset. AssetBundleListings are meant to be used at runtime,
-	so they cannot strongly reference the AssetBundleContents without dragging them into the build.*/
-[System.Serializable]
-public class AssetBundleContentsWeakReference {
-	public string tags; //Period-delimited tag set string
-
-#if UNITY_EDITOR
-	public string ContentsPath {
-		get { return contentsPath; }		
-		set{ contentsPath = value; }
-	}
-#endif
-	[SerializeField] private string contentsPath; //Path to AssetBundleContents asset
-
-#if UNITY_EDITOR
-	public AssetBundleContents Load(){
-		return AssetDatabase.LoadMainAssetAtPath(contentsPath) as AssetBundleContents;
-	}
-	
-	public AssetBundleContents LoadOrCreate(AssetBundleListing sourceListing){
-		if(string.IsNullOrEmpty(contentsPath)){
-			Create(sourceListing);
-		}
-		AssetBundleContents loadedContents = Load();
-		if(!loadedContents){
-			Debug.LogWarning("Missing AssetBundleContents at \"" + contentsPath + "\". A replacement will be created.");
-			Create(sourceListing);
-			loadedContents = Load();
-		}
-		else{
-			System.Console.WriteLine("Loaded AssetBundleContents at " + contentsPath + " for " + sourceListing + " with tags \"" + tags + "\"");
-		}
-		return loadedContents;
-	}
-	
-	public void Create(AssetBundleListing sourceListing){
-		System.Console.WriteLine("Creating AssetBundleContents for " + sourceListing + " with tags \"" + tags + "\"");
-		contentsPath = GetPath(sourceListing.Id, tags);
-		if(!Directory.Exists(Path.GetDirectoryName(contentsPath))){
-			Directory.CreateDirectory(Path.GetDirectoryName(contentsPath));
-		}
-		AssetBundleContents	contents = ScriptableObject.CreateInstance<AssetBundleContents>();
-		contents.listing = sourceListing;
-		contents.tags = tags;
-		AssetDatabase.CreateAsset(contents, contentsPath);
-		EditorUtility.SetDirty(sourceListing);
-		AssetDatabase.SaveAssets();
-	}
-	
-	//Returns the target path for a contents with the given listing ID and tag set.
-	public static string GetPath(string listingId, string tags){
-		string dir = Path.Combine(AssetBundleEditorSettings.DirectoryPath, "BundleContents");
-		return Path.Combine(dir, listingId + (string.IsNullOrEmpty(tags) ? "" : AssetBundleChars.BundleSeparator.ToString()) + tags + ".asset");
 	}
 #endif
 }
